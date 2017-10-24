52d09ea3f7e57570f7b8f476e0decfe04b7f5441
Luiz0tavio <luizotavioresende@hotmail.com>
2017-10-24 19:13:28 -0200

    common::protocol

---
+++ common/Protocol.hpp
@@ -0,0 +1,23 @@
+#ifndef TCP_CHAT_PROTOCOL_HPP
+#define TCP_CHAT_PROTOCOL_HPP
+
+#include <cstdint>
+
+namespace Common {
+
+    class Protocol {
+
+    private:
+
+        uint16_t type;
+        uint16_t id;
+        uint16_t dest;
+        uint16_t seq;
+
+    public:
+        Protocol(uint16_t _type, uint16_t id, uint16_t dest, uint16_t seq): type(_type), id(id), dest(dest), seq(seq){}
+    };
+
+}
+
+#endif //TCP_CHAT_PROTOCOL_HPP
