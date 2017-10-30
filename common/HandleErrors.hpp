9aa3e24c4dca7f3e7aa44a6df882c9e1cbc7aecf
Luiz Otavio <me@luizotav.io>
2017-10-30 11:08:46 +0000

    HandleErros class

---
+++ common/HandleErrors.hpp
@@ -0,0 +1,10 @@
+#ifndef TCP_CHAT_HANDLEERRORS_HPP
+#define TCP_CHAT_HANDLEERRORS_HPP
+
+namespace Common {
+    class HandleErrors {
+                   
+    };
+}
+
+#endif //TCP_CHAT_HANDLEERRORS_HPP
--- server/main.cpp
+++ server/main.cpp
@@ -1,3 +1,9 @@
+#include <iostream>
+#include <memory>
+
 int main(int argc, char* argv[]) {
+    {
+        std::unique_ptr<>
+    }
     return 0;
 }
