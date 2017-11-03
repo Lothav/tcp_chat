405cd1cacd4a27d0338833d8f7fb36e20369e385
Luiz0tavio <luizotavioresende@hotmail.com>
2017-11-03 15:58:35 -0200

    select function watch events

--- client/Module.hpp
+++ client/Module.hpp
@@ -18,30 +18,35 @@ namespace Client {
     public:
         explicit Module(std::string port)
         {
+            int socket_server = this->getSocket();
+
             struct sockaddr_in dst = {};
             dst.sin_family  = AF_INET;
             dst.sin_port    = htons(static_cast<uint16_t>(std::stoi(port.c_str())));
             inet_aton(LOCAL_HOST, &dst.sin_addr);
 
             auto *sa_dst = (struct sockaddr *)&dst;
-            while(connect(this->getSocket(), sa_dst, sizeof(dst))) {}
+            while(connect(socket_server, sa_dst, sizeof(dst)));
 
             fd_set rfds = {};
             struct timeval tv = {};
             char buf[35];
 
-            while(1) {
-                FD_ZERO(&rfds);                     // Clear an fd_set
-                FD_SET(STDIN_FILENO, &rfds);        // Add a descriptor to an fd_set
-                FD_SET(this->getSocket(), &rfds);
-                tv.tv_sec = 5;
-                tv.tv_usec = 0;
-                int  retval = select(3, &rfds, nullptr, nullptr, &tv);
+            FD_ZERO(&rfds);                     // Clear an fd_set
+            FD_SET(STDIN_FILENO, &rfds);        // Add a descriptor to an fd_set
+            FD_SET(socket_server, &rfds);
+            tv.tv_sec = 5;
+            tv.tv_usec = 0;
+
+            while(true) {
+                int retval = select(socket_server+1, &rfds, nullptr, nullptr, &tv);
                 if (FD_ISSET(STDIN_FILENO, &rfds)) {
                     if (fgets(buf, 35, stdin)) {
                         printf("%d %s\n",__LINE__ ,buf);
                     }
-                    printf("\nEnter command: ");
+                }
+                if (FD_ISSET(socket_server, &rfds)) {
+                    int ret = recv(socket_server, buf, 4, 0);
                 }
             }
         }
--- server/Module.hpp
+++ server/Module.hpp
@@ -8,6 +8,7 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <zconf.h>
+#include <cstring>
 #include "../common/Socket.hpp"
 
 #define TC_MAX_REQUESTS 5
@@ -25,25 +26,26 @@ namespace Server {
             if (setsockopt(_socket_server, SOL_SOCKET, SO_REUSEADDR, &size, sizeof(int)) < 0)
                 printf("setsockopt(SO_REUSEADDR) failed");
 
-            struct sockaddr_in 	serv_addr = {};
+            struct sockaddr_in serv_addr = {};
             serv_addr.sin_family = AF_INET;
             serv_addr.sin_port   = htons(static_cast<uint16_t>(std::atoi(port)));
             inet_aton(LOCAL_HOST, &serv_addr.sin_addr);
 
-            while (bind(_socket_server, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {}
+            while (bind(_socket_server, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0);
 
-            listen(this->getSocket(), TC_MAX_REQUESTS);
+            listen(_socket_server, TC_MAX_REQUESTS);
 
             struct sockaddr_in 	cli_addr = {};
             socklen_t clilen = sizeof(cli_addr);
 
             int _socket_client = accept(_socket_server, (struct sockaddr *)&cli_addr, &clilen);
-
-
+            while (1) {
+                char buffer[4] = "fas";
+                send(_socket_client, &buffer, strlen(buffer), 0);
+                sleep(1);
+            }
         }
-
     };
-
 }
 
 #endif //TCP_CHAT_MODULE_HPP
